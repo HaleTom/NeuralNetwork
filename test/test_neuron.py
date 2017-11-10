040cb74ec311b067b9845eb818d153d9a2379a61
Tom "Ravi" Hale <tom@hale.ee>
2017-11-10 20:22:36 +0700

    Correct name of two_weights to to_inputs

--- test/test_neuron.py
+++ test/test_neuron.py
@@ -1,17 +1,17 @@
-import pytest
+from pytest import fixture, raises
 from neuron import Neuron, InsufficientInput
 from activation_functions import sigmoid
 
 
-@pytest.fixture
+@fixture
 def three_weights_plus_bias():
     """Returns a Neuron with weights of (-1, 2, -3) and bias of 4"""
     return Neuron([-1, 2, -3], bias=4, activation_func=sigmoid)
 
 
-@pytest.fixture
-def two_weights():
-    """Returns two weights"""
+@fixture
+def two_inputs():
+    """Returns two inputs"""
     return [1, 1]
 
 
@@ -26,10 +26,10 @@ def test_weighted_sum_neuron(three_weights_plus_bias):
 
 
 def test_weighted_sum_inputs_not_matching_weights(
-        three_weights_plus_bias, two_weights):
-    assert len(two_weights) != len(three_weights_plus_bias.weights.tolist())
-    with pytest.raises(InsufficientInput):
-        three_weights_plus_bias.weighted_sum(two_weights)
+        three_weights_plus_bias, two_inputs):
+    assert len(two_inputs) != len(three_weights_plus_bias.weights.tolist())
+    with raises(InsufficientInput):
+        three_weights_plus_bias.weighted_sum(two_inputs)
 
 
 def test_output(three_weights_plus_bias):
